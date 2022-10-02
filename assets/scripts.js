4b473c5178ab1563aed88a9ac1aff34bf8294c3a
nhartm <natalie.hartmann98@gmail.com>
2022-10-02 15:30:52 -0700

    Updated the local storage, fixed error message

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,113 +1,102 @@
 var searchBar = document.querySelector("#search-form");
 var city;
 var beer = document.querySelector("#save-your-brews");
-var saveButton = document.querySelector("save-button");
+var saveButton = document.querySelector("#save-button");
 
-//renderFavoriteBeers();
+
 
 function initMap() {
 
 
-  map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: new google.maps.LatLng(38.685516, -101.073324),
-    mapTypeId: "terrain",
-    gestureHandling: "cooperative",
-  });
-  
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: new google.maps.LatLng(38.685516, -101.073324),
+        mapTypeId: "terrain",
+        gestureHandling: "cooperative",
+    });
+
 }
 // Create a <script> tag and set the USGS URL as the source.
 function getCity(city) {
-  var city = $("#search-form").val()
-  var requestUrl = `https://api.openbrewerydb.org/breweries?by_city=${city}`;
-  console.log(city)
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data)
-      // Loop through the results array and place a marker for each
-      // set of coordinates.
-      // const eqfeed_callback = function (results) 
+    var city = $("#search-form").val()
+    var requestUrl = `https://api.openbrewerydb.org/breweries?by_city=${city}`;
+    console.log(city)
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+            // Loop through the results array and place a marker for each
+            // set of coordinates.
+            // const eqfeed_callback = function (results) 
 
-      for (let i = 0; i < data.length; i++) {
-        var lat = data[i].latitude
-        var lon = data[i].longitude
-        var street = data[i].street
-        var name = data[i].name
-        var type = data[i].brewery_type
-        // const coords = results.features[i].geometry.coordinates;
-        const latLng = new google.maps.LatLng(lat, lon);
-        // console.log(lat)
-        // console.log(lon)
-        var streetDisplay = $("<p>").append("Street location: ", street)
-        var nameLocal = $("<p>").append("Name: ", name)
-        var brewType = $("<p>").append("Type: ", type)
+            for (let i = 0; i < data.length; i++) {
+                var lat = data[i].latitude
+                var lon = data[i].longitude
+                var street = data[i].street
+                var name = data[i].name
+                var type = data[i].brewery_type
+                // const coords = results.features[i].geometry.coordinates;
+                const latLng = new google.maps.LatLng(lat, lon);
+                // console.log(lat)
+                // console.log(lon)
+                var streetDisplay = $("<p>").append("Street location: ", street)
+                var nameLocal = $("<p>").append("Name: ", name)
+                var brewType = $("<p>").append("Type: ", type)
 
 
-        // $("#searchResults").empty()
-        $("#searchResults").append(nameLocal);
-        $("#searchResults").append(brewType);
-        $("#searchResults").append(streetDisplay);
+                // $("#searchResults").empty()
+                $("#searchResults").append(nameLocal);
+                $("#searchResults").append(brewType);
+                $("#searchResults").append(streetDisplay);
 
-        new google.maps.Marker({
-          position: latLng,
-          map: map,
+                new google.maps.Marker({
+                    position: latLng,
+                    map: map,
+                });
+
+            };
+
+
+            //     for (var i = 0; i < data.length; i++) {
+            //     }
+
         });
-        
-      };
-
-
-      //     for (var i = 0; i < data.length; i++) {
-      //     }
-
-    });
 }
 
-// renderFavoriteBeers();
-
-// function displaySavedBrews (type) {
-//     document.querySelector("#saved-brews").textContent = beer;
-//     document.querySelector("#saved-brews").setAttribute("class", type);
-// }
-
-// function renderFavoriteBeers() {
-//     var beer = localStorage.getItem ("beerName");
-//     if (!beerName) {
-//         return;
-//     }
-//     beer.textContent = beer;
-// }
-
-// saveButton.addEventListener("click", function(event){
-//     event.preventDefault();
-
-// // Add favorite beers to local storage
-//  var beerName = document.querySelector("#save_your_brews").value;
-
-//  if(beerName === "") {
-//     displayMessage("error", "Please enter the name of the beer you would like to save");
-//  } else {
-//     displayMessage("success", "Beer has been saved");
- 
-//     localStorage.setItem("beerName", beerName);
-//     renderFavoriteBeers();
-//}
-//});
 
 
-$( "#clear" ).click(function() {
-    $( "#searchResults" ).empty();
-   })
-   
-   
-   
-   
-   
-   
-   
-   
+function displaySavedBrews(type) {
+    document.querySelector("#saved-brews").textContent = beer;
+    document.querySelector("#saved-brews").setAttribute("class", type);
+}
+
+
+
+saveButton.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    // Add favorite beers to local storage
+    var beerName = beer.value;
+    console.log(beerName)
+    localStorage.setItem("beerName", beerName);
+    renderFavoriteBeers();
+    function renderFavoriteBeers() {
+        var beer = localStorage.getItem("beerName");
+        if (!beerName) {
+            return;
+        }
+        beer.textContent = beer;
+    }
+
+});
+
+
+
+$("#clear").click(function () {
+    $("#searchResults").empty();
+})
 
 window.initMap = initMap;
 // window.eqfeed_callback = eqfeed_callback;
